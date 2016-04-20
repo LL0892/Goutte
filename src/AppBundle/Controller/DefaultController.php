ad2cffee60d05b60a7315dceece59bfa394d5b24
Laurent Lienher <laurent.lienher@iomedia.ch>
2016-04-20 11:24:43 +0200

    Refactor

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,6 +19,7 @@ class DefaultController extends Controller
     {
         $client = new Client();
         $query = new Query();
+        $totalResult = null;
 
 
         $searchForm = $this->createFormBuilder($query)
@@ -33,124 +34,132 @@ class DefaultController extends Controller
 
 
         $postData = $request->request->all();
-        $searchQuery = $postData['form']['search'];
+
+        if (count($postData) <= 0) {
+            $searchQuery = null;
+        } else {
+            $searchQuery = $postData['form']['search'];
+        }
+
         //$searchQuery = 'PANASONIC DMC-LX100';
 
+        if (count($postData) > 0) {
+            /*
+             * HEINIGERAG.CH
+             */
+            $baseUrl = 'http://shop.heinigerag.ch/';
+            $crawler = $client->request('GET', $baseUrl);
+            $form = $crawler->filter('#quicksearch')->first()->form();
+            $crawler = $client->submit($form, array(
+                'q' => $searchQuery,
+                'sort' => 'relevance|desc'
+            ));
 
-        /*
-         * HEINIGERAG.CH
-         */
-        $baseUrl = 'http://shop.heinigerag.ch/';
-        $crawler = $client->request('GET', $baseUrl);
-        $form = $crawler->filter('#quicksearch')->first()->form();
-        $crawler = $client->submit($form, array(
-            'q' => $searchQuery,
-            'sort' => 'relevance|desc'
-        ));
+            $data = $crawler->filter('div.item.media')->each(function ($node) {
+                $name = $node->filter('h4.media-heading > a')->text();
+                $subUrl = $node->filter('h4.media-heading > a')->attr('href');
+                $desc = $node->filter('span.productName')->text();
+                $price = $node->filter('span.price')->text();
+                $image = $node->filter('div.media-left > a > img')->attr('src');
 
-        $data = $crawler->filter('div.item.media')->each(function ($node) {
-            $name = $node->filter('h4.media-heading > a')->text();
-            $subUrl = $node->filter('h4.media-heading > a')->attr('href');
-            $desc = $node->filter('span.productName')->text();
-            $price = $node->filter('span.price')->text();
-            $image = $node->filter('div.media-left > a > img')->attr('src');
+                $data = array(
+                    'name' => $name,
+                    'url' => $subUrl,
+                    'price' => $price,
+                    'desc' => $desc,
+                    'image' => $image
+                );
 
-            $data = array(
-                'name' => $name,
-                'url' => $subUrl,
-                'price' => $price,
-                'desc' => $desc,
-                'image' => $image
+                return $data;
+            });
+
+            $result = array(
+                'baseUrl' => $baseUrl,
+                'data' => $data,
+                'dataCount' => count($data)
             );
-
-            return $data;
-        });
-
-        $result = array(
-            'baseUrl' => $baseUrl,
-            'data' => $data,
-            'dataCount' => count($data)
-        );
-        $totalResult[] = $result;
+            $totalResult[] = $result;
 
 
-        /*
-         * MELECTRONICS.CH
-         */
-        $baseUrl = 'http://www.melectronics.ch/fr/';
-        $crawler = $client->request('GET', $baseUrl);
-        $form = $crawler->filter('#searchbox')->first()->form();
-        $crawler = $client->submit($form, array(
-           'q' => $searchQuery
-        ));
+            /*
+             * MELECTRONICS.CH
+             */
+            $baseUrl = 'http://www.melectronics.ch/fr/';
+            $crawler = $client->request('GET', $baseUrl);
+            $form = $crawler->filter('#searchbox')->first()->form();
+            $crawler = $client->submit($form, array(
+                'q' => $searchQuery
+            ));
 
-        $data = $crawler->filter('div.listing > ul > li')->each(function ($node) {
-            $name = $node->filter('h3.productname')->text();
-            $subUrl = $node->filter('div.productcell > div.content > a')->attr('href');
-            $desc = $node->filter('span.topfacts')->text();
-            $price = $node->filter('span.price > span.current')->text();
-            $image = $node->filter('span.product > img')->attr('src');
+            $data = $crawler->filter('div.listing > ul > li')->each(function ($node) {
+                $name = $node->filter('h3.productname')->text();
+                $subUrl = $node->filter('div.productcell > div.content > a')->attr('href');
+                $desc = $node->filter('span.topfacts')->text();
+                $price = $node->filter('span.price > span.current')->text();
+                $image = $node->filter('span.product > img')->attr('src');
 
-            $data = array(
-                'name' => $name,
-                'url' => $subUrl,
-                'price' => $price,
-                'desc' => $desc,
-                'image' => $image
+                $data = array(
+                    'name' => $name,
+                    'url' => $subUrl,
+                    'price' => $price,
+                    'desc' => $desc,
+                    'image' => $image
+                );
+
+                return $data;
+            });
+
+            $result = array(
+                'baseUrl' => $baseUrl,
+                'data' => $data,
+                'dataCount' => count($data)
             );
+            $totalResult[] = $result;
 
-            return $data;
-        });
 
-        $result = array(
-            'baseUrl' => $baseUrl,
-            'data' => $data,
-            'dataCount' => count($data)
-        );
-        $totalResult[] = $result;
+            /*
+             * HAWK.CH
+             */
+            $baseUrl = 'http://www.hawk.ch';
+            $crawler = $client->request('GET', $baseUrl);
+            $form = $crawler->filter('#search_mini_form')->first()->form();
+            $crawler = $client->submit($form, array(
+                'q' => $searchQuery
+            ));
 
+            $data = $crawler->filter('ul.products-grid > li.item')->each(function ($node) {
+                $name = $node->filter('h2.product-name a')->attr('title');
+                $subUrl = $node->filter('h2.product-name > a')->attr('href');
+                $desc = null;
+                $price = $node->filter('span.price')->text();
+                $image = $node->filter('a.product-image > img')->attr('src');
+
+                $data = array(
+                    'name' => $name,
+                    'url' => $subUrl,
+                    'price' => $price,
+                    'desc' => $desc,
+                    'image' => $image
+                );
+
+                return $data;
+            });
+
+            $result = array(
+                'baseUrl' => $baseUrl,
+                'data' => $data,
+                'dataCount' => count($data)
+            );
+            $totalResult[] = $result;
+
+            //dump($totalResult);
+            //print $crawler->html();
+            //dump($form);
+            //dump($crawler);
+            //dump($client->getResponse()->getContent());
+            //exit;
+        }
         
-        /*
-         * HAWK.CH
-         */
-        $baseUrl = 'http://www.hawk.ch';
-        $crawler = $client->request('GET', $baseUrl);
-        $form = $crawler->filter('#search_mini_form')->first()->form();
-        $crawler = $client->submit($form, array(
-            'q' => $searchQuery
-        ));
-
-        $data = $crawler->filter('ul.products-grid > li.item')->each(function ($node) {
-            $name = $node->filter('h2.product-name a')->attr('title');
-            $subUrl = $node->filter('h2.product-name > a')->attr('href');
-            $desc = null;
-            $price = $node->filter('span.price')->text();
-            $image = $node->filter('a.product-image > img')->attr('src');
-
-            $data = array(
-                'name' => $name,
-                'url' => $subUrl,
-                'price' => $price,
-                'desc' => $desc,
-                'image' => $image
-            );
-
-            return $data;
-        });
-
-        $result = array(
-            'baseUrl' => $baseUrl,
-            'data' => $data,
-            'dataCount' => count($data)
-        );
-        $totalResult[] = $result;
-        
-        //dump($totalResult);
-        //print $crawler->html();
-        //dump($form);
-        //dump($crawler);
-        //dump($client->getResponse()->getContent());
-        //exit;
 
         return $this->render('AppBundle:Default:index.html.twig', array(
             'results' => $totalResult,
